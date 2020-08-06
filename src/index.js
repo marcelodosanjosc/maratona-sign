d47bb924a400eb703a967d8ee1b9dd5d835934a3
marcelodosanjosc <marcelodosanjosc@gmail.com>
2020-08-06 07:57:24 -0400

    criacao do account e banco

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 
+const db = require('./models')
+
+const authController = require('./controllers/auth')
+
 const app = express()
+
+app.use('/auth', authController)
 
 app.get('/', (req, res) =>{
     return res.json('Api running..')
 })
 
-app.listen(3001, () => {
-    console.log('Listening on post 3001');
-})
+    db.sequelize.sync().then(()=> {
+    app.listen(3001, () => {
+        console.log('Listening on post 3001');
+    })
+    })
